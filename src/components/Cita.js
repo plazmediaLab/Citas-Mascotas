fa53982a0c185a59441fd178b76bf72305ed0e39
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-01-24 20:45:36 -0600

    Eliminar cita con id del Array CITAS

--- src/components/Cita.js
+++ src/components/Cita.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 
 
-const Cita = ({cita}) => (
+const Cita = ({cita, eliminarCita}) => (
     <Fragment>
       <div className="card mb-2">
         <p>Mascota: <span>{ cita.mascota }</span></p>
@@ -9,6 +9,19 @@ const Cita = ({cita}) => (
         <p>Fecha de alta: <span>{ cita.fecha }</span></p>
         <p>Hora de ingreso: <span>{ cita.hora }</span></p>
         <p>Síntomas: <span>{ cita.sintomas }</span></p>
+
+        {/* <p>Mascota: <span>{  }</span></p>
+        <p>Dueño: <span>{  }</span></p>
+        <p>Fecha de alta: <span>{  }</span></p>
+        <p>Hora de ingreso: <span>{  }</span></p>
+        <p>Síntomas: <span>{  }</span></p> */}
+        <hr />
+        <div className="jc-end">
+          <button
+            className="btn btn-s-br btn-cancel"
+            onClick={ () => eliminarCita(cita.id) }
+          ><i className="a-trash-o"></i></button>
+        </div>
       </div> 
     </Fragment>
   );
